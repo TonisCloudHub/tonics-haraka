848e9d69e7c67e911d940e3abccceaa49f5ac338
Olayemi Faruq <37757164+devsrealm@users.noreply.github.com>
2024-04-01 01:16:35 +0100

    Update script.sh
    Replaced lxc with incus

--- .github/bookworm/scripts/script.sh
+++ .github/bookworm/scripts/script.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
+# Init incus
+sudo incus admin init --auto
+
 # Launch Instance
-lxc launch images:debian/bookworm/amd64 tonics-haraka
+sudo incus launch images:debian/bookworm/amd64 tonics-haraka
 
 # Dependencies
-lxc exec tonics-haraka -- bash -c "apt update -y && apt install build-essential -y"
+sudo incus exec tonics-haraka -- bash -c "apt update -y && apt install build-essential -y"
 
-lxc exec tonics-haraka -- bash -c "apt install curl whois -y && curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && apt-get install -y nodejs"
+sudo incus exec tonics-haraka -- bash -c "apt install curl whois -y && curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && apt-get install -y nodejs"
 
 # Install Haraka
-lxc exec tonics-haraka -- bash -c "npm install -g Haraka"
+sudo incus exec tonics-haraka -- bash -c "npm install -g Haraka"
 
 # Check Haraka Version 
-lxc exec tonics-haraka -- haraka -v
+sudo incus exec tonics-haraka -- haraka -v
 
 # Instance of Haraka
-lxc exec tonics-haraka -- haraka -i tonics_haraka
+sudo incus exec tonics-haraka -- haraka -i tonics_haraka
 
 # Haraka AuthEnc Plugin
-lxc exec tonics-haraka -- npm install haraka-plugin-auth-enc-file
+sudo incus exec tonics-haraka -- npm install haraka-plugin-auth-enc-file
 
 # Install Certbot for Standalone Certificate Generation
-lxc exec tonics-haraka -- apt-get -y install certbot
+sudo incus exec tonics-haraka -- apt-get -y install certbot
 
 # Necessary Files
-lxc exec tonics-haraka -- bash -c "touch /root/tonics_haraka/config/tls.ini && touch /root/tonics_haraka/config/auth_enc_file.ini"
+sudo incus exec tonics-haraka -- bash -c "touch /root/tonics_haraka/config/tls.ini && touch /root/tonics_haraka/config/auth_enc_file.ini"
 
 # Haraka Plugins Setup
 cat << EOF | sudo tee -a tonics_haraka.plugins
@@ -40,10 +43,10 @@ queue/smtp_forward
 haraka-plugin-auth-enc-file
 EOF
 
-lxc file push tonics_haraka.plugins tonics-haraka/root/tonics_haraka/config/plugins
+sudo incus file push tonics_haraka.plugins tonics-haraka/root/tonics_haraka/config/plugins
 
 # SystemD Manager
-lxc exec tonics-haraka -- touch  /etc/systemd/system/tonics_haraka.service
+sudo incus exec tonics-haraka -- touch  /etc/systemd/system/tonics_haraka.service
 
 cat << EOF | sudo tee -a tonics_haraka.service
 [Unit]
@@ -61,22 +64,22 @@ User=root
 WantedBy=multi-user.target
 EOF
 
-lxc file push tonics_haraka.service tonics-haraka/etc/systemd/system/tonics_haraka.service
+sudo incus file push tonics_haraka.service tonics-haraka/etc/systemd/system/tonics_haraka.service
 
-lxc exec tonics-haraka -- systemctl daemon-reload
+sudo incus exec tonics-haraka -- systemctl daemon-reload
 
 # Clean Debian Cache
-lxc exec tonics-haraka -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
+sudo incus exec tonics-haraka -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
 
 # Haraka Version
-Version=$(lxc exec tonics-haraka -- haraka -v | grep -oP "Version: \K[\d.]+")
+Version=$(sudo incus exec tonics-haraka -- haraka -v | grep -oP "Version: \K[\d.]+")
 
 # Publish Image
-mkdir images && lxc stop tonics-haraka && lxc publish tonics-haraka --alias tonics-haraka
+mkdir images && sudo incus stop tonics-haraka && sudo incus publish tonics-haraka --alias tonics-haraka
 
 # Export Image
-lxc start tonics-haraka
-lxc image export tonics-haraka images/haraka-bookworm-$Version
+sudo incus start tonics-haraka
+sudo incus image export tonics-haraka images/haraka-bookworm-$Version
 
 # Image Info
-lxc image info tonics-haraka >> images/info.txt
+sudo incus image info tonics-haraka >> images/info.txt
